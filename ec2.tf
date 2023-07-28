f5f472c98ee3e28f9f9f3fd5adaf2bc659f1365b
Aravind <kalavida28@gmail.com>
2023-07-29 01:48:07 +0530

    Added provisioners right way

--- ec2.tf
+++ ec2.tf
@@ -96,7 +96,7 @@ resource "aws_security_group" "Kubernetes" {
   }
 }
 
-# Create Instance Kube Master
+# Create Instance
 
 resource "aws_instance" "Kubernetes_Master" {
   ami           = "ami-03cb1380eec7cc118"
@@ -111,35 +111,7 @@ resource "aws_instance" "Kubernetes_Master" {
   }
 }
 
-#The purpose of this below code is to copy your Ansible public key (specified by source) to the remote EC2 instance 
-#before the subsequent remote-exec provisioner block runs. 
-#The remote-exec block will then use the copied public key to set up the ansible-admin user's SSH access.
-
-  provisioner "remote-exec" {
-      inline = [
-        "sudo adduser ansible-admin --disabled-password --gecos ''",
-        "sudo usermod -aG sudo ansible-admin",
-        "echo 'ansible-admin ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible-admin",
-      ]
-  }
-
-  provisioner "file" {
-    source      = "/home/devopsadmin/.ssh/id_rsa.pub"  # Update this path to your actual public key file path on the Ansible Controller
-    destination = "/tmp/id_rsa.pub"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /home/ansible-admin/.ssh",
-      "sudo mv /tmp/id_rsa.pub /home/ansible-admin/.ssh/authorized_keys",
-      "sudo chown -R ansible-admin:ansible-admin /home/ansible-admin/.ssh",
-      "sudo chmod 700 /home/ansible-admin/.ssh",
-      "sudo chmod 600 /home/ansible-admin/.ssh/authorized_keys"
-    ]
-  }
-
-
-# Create Instance Kube worker 1
+# Create Instance 
 
 resource "aws_instance" "Kubernetes_Workernode1" {
   ami           = "ami-03cb1380eec7cc118"
@@ -153,29 +125,9 @@ resource "aws_instance" "Kubernetes_Workernode1" {
     Name = "Kubernetes_Workernode1"
   }
 }
-  provisioner "remote-exec" {
-      inline = [
-        "sudo adduser ansible-admin --disabled-password --gecos ''",
-        "sudo usermod -aG sudo ansible-admin",
-        "echo 'ansible-admin ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible-admin",
-      ]
-  }
 
-  provisioner "file" {
-    source      = "/home/devopsadmin/.ssh/id_rsa.pub"  # Update this path to your actual public key file path on the Ansible Controller
-    destination = "/tmp/id_rsa.pub"
-  }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /home/ansible-admin/.ssh",
-      "sudo mv /tmp/id_rsa.pub /home/ansible-admin/.ssh/authorized_keys",
-      "sudo chown -R ansible-admin:ansible-admin /home/ansible-admin/.ssh",
-      "sudo chmod 700 /home/ansible-admin/.ssh",
-      "sudo chmod 600 /home/ansible-admin/.ssh/authorized_keys"
-    ]
-  }
-# Create Instance Kube worker 2
+# Create Instance
 
 resource "aws_instance" "Kubernetes_Workernode2" {
   ami           = "ami-03cb1380eec7cc118"
@@ -188,35 +140,4 @@ resource "aws_instance" "Kubernetes_Workernode2" {
   tags = {
     Name = "Kubernetes_Workernode2"
   }
-}
-  provisioner "remote-exec" {
-      inline = [
-        "sudo adduser ansible-admin --disabled-password --gecos ''",
-        "sudo usermod -aG sudo ansible-admin",
-        "echo 'ansible-admin ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible-admin",
-      ]
-  }
-
-  provisioner "file" {
-    source      = "/home/devopsadmin/.ssh/id_rsa.pub"  # Update this path to your actual public key file path on the Ansible Controller
-    destination = "/tmp/id_rsa.pub"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir -p /home/ansible-admin/.ssh",
-      "sudo mv /tmp/id_rsa.pub /home/ansible-admin/.ssh/authorized_keys",
-      "sudo chown -R ansible-admin:ansible-admin /home/ansible-admin/.ssh",
-      "sudo chmod 700 /home/ansible-admin/.ssh",
-      "sudo chmod 600 /home/ansible-admin/.ssh/authorized_keys"
-    ]
-  }
-
-#output the IPs of instances created 
-output "instance_ips" {
-  value = [
-    aws_instance.Kubernetes_Master.public_ip,
-    aws_instance.Kubernetes_Workernode1.public_ip,
-    aws_instance.Kubernetes_Workernode2.public_ip
-  ]
 }
